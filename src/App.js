b11fd7a101f1177cf9a8b284c8812e39e5a55185
TuovinenIlari <ilari.tuovinen@hotmail.com>
2022-10-08 13:31:26 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import NavBar from './components/NavBar';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar></NavBar>
+      <p>TEEEEEEST</p>
+    </div>
+  );
+}
+
+export default App;
