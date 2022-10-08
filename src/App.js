60f11488c068c23bfe5cb1c56b5fb47ec64768ea
Ilari Tuovinen <ilari.tuovinen@hotmail.com>
2022-10-08 13:55:14 +0300

    Routes to app.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
-import NavBar from './components/NavBar';
-import './App.css';
+import NavBar from "./components/NavBar";
+import "./App.css";
+import About from "./pages/About";
+import ContactUs from "./pages/ContactUs";
+import Home from "./pages/Home";
+import NotFound from "./pages/NotFound";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <NavBar></NavBar>
-      <p>TEEEEEEST</p>
-    </div>
+    <>
+      <NavBar />
+      <Header />
+      <div className="Container">
+        <Routes>
+          <Route path="/" element={<Home></Home>} />
+          <Route path="/about" element={<About></About>} />
+          <Route path="/contactus" element={<ContactUs></ContactUs>} />
+          <Route path="*" element={<NotFound></NotFound>} />
+        </Routes>
+      </div>
+      <Footer />
+    </>
   );
 }
 
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -12,10 +12,13 @@ export default function NavBar() {
                 <div class="collapse navbar-collapse" id="navbarsExample02">
                     <ul class="navbar-nav me-auto">
                         <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="#">Home</a>
+                            <Link className="nav-link" to="/">Home</Link>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="#">Link</a>
+                        <Link className="nav-link" to="/about">About</Link>
+                        </li>
+                        <li class="nav-item">
+                        <Link className="nav-link" to="/contactus">contact Us</Link>
                         </li>
                     </ul>
                 </div>
